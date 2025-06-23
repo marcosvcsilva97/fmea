b7263f7d7d197ea30ac20e9886e21ce6db35bf7b
Marcos Silva <mvcosta.silva1997@gmail.com>
2025-06-23 07:54:38 -0300

    Correções Pontuais

--- script.js
+++ script.js
@@ -53,3 +53,25 @@ function prevPage() {
 }
 
 renderTable();
+
+// Modal
+const switchModal = () => {
+    const modal = document.querySelector('.modal')
+    const actualStyle = modal.style.display
+    if(actualStyle == 'block') {
+      modal.style.display = 'none'
+    }
+    else {
+      modal.style.display = 'block'
+    }
+  }
+  
+  const btn = document.querySelector('.modalBtn')
+  btn.addEventListener('click', switchModal)
+  
+  window.onclick = function(event) {
+      const modal = document.querySelector('.modal')
+    if (event.target == modal) {
+      switchModal()
+    }
+  }
